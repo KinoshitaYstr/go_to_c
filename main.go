5cbcfa0c9452af8b5a3207869a34eb6310f03bb1
KinoshitaYstr <photo_18@outlook.jp>
2020-09-30 14:44:14 +0900

    tashizan hikizan dekita zogit add .

--- main.go
+++ main.go
@@ -6,6 +6,20 @@ import (
 	"os"
 )
 
+// 文字列からfloat64を取得して、読み取ったものを飛ばして返すもの
+func GetNumberString(data string) (string, string) {
+	result := ""
+	for {
+		if len(data) != 0 && '0' <= data[0] && data[0] <= '9' {
+			result += string(data[0])
+			data = data[1:]
+		} else {
+			break
+		}
+	}
+	return result, data
+}
+
 func main() {
 	// コマンドライン引数関係
 	flag.Parse()
@@ -19,7 +33,27 @@ func main() {
 	fmt.Println(".intel_syntax noprefix")
 	fmt.Println(".global main")
 	fmt.Println("main:")
-	fmt.Println("  mov rax, " + args[0])
+	// 値を取る
+	arg := args[0]
+	var val string
+	for len(arg) > 0 {
+		if arg[0] == '+' {
+			arg = arg[1:]
+			val, arg = GetNumberString(arg)
+			fmt.Println("  add rax, " + val)
+			continue
+		}
+		if arg[0] == '-' {
+			arg = arg[1:]
+			val, arg = GetNumberString(arg)
+			fmt.Println("  sub rax, " + val)
+			continue
+		}
+		val, arg = GetNumberString(arg)
+		if len(val) != 0 {
+			fmt.Println("  mov rax, " + val)
+		}
+	}
 	fmt.Println("  ret")
 	return
 }
