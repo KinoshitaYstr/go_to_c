9052143b5df6b7ada5b9c9fef65b80a14d3fcb07
KinoshitaYstr <photo_18@outlook.jp>
2020-09-30 11:38:25 +0900

    komando rain hikisuu kara atai wo shitei dekita

--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
+	// コマンドライン引数関係
+	flag.Parse()
+	args := flag.Args()
+	// 引数の確認(なかったらエラー)
+	if len(args) != 1 {
+		fmt.Fprintln(os.Stderr, "引数の個数が正しくありません")
+		return
+	}
+	// 実際の処理
 	fmt.Println(".intel_syntax noprefix")
 	fmt.Println(".global main")
 	fmt.Println("main:")
-	fmt.Println("  mov rax, 42")
+	fmt.Println("  mov rax, " + args[0])
 	fmt.Println("  ret")
+	return
 }
